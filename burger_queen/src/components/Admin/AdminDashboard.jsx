3679eb33a0c65c218bcbeda06316828eed3786e4
DenisseMolina <dmolinaruelas@gmail.com>
2022-05-16 13:53:36 -0500

    First version of router dom and first components

---
+++ burger_queen/src/components/Admin/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+import './admin.css'
+
+export default function AdminDashboard() {
+  return (
+    <div>
+      <h1>AQUI VA ADMIN</h1>
+    </div>
+  );
+}
---
+++ burger_queen/src/components/Chef/ChefDashboard.jsx
@@ -0,0 +1,11 @@
+import './chef.css';
+
+export default function ChefDashboard (){
+    return(
+        <div>
+            <h1>AQUI VA CHEF MURA</h1>
+              
+            
+        </div>
+    )
+}
