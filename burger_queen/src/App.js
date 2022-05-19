ca50ae828a4922842fe8155ea4273db4451e085f
DenisseMolina <dmolinaruelas@gmail.com>
2022-05-19 12:50:33 -0500

    New components of view home

--- burger_queen/src/App.js
+++ burger_queen/src/App.js
@@ -6,12 +6,14 @@ import WaiterDashboard from "./views/Waiter/WaiterDashboard";
 import Order from "./views/Waiter/Order";
 import PreparingOrder from "./views/Waiter/Preparing";
 import ChefDashboard from "./views/Chef/ChefDashboard";
+import Home from "./views/Login/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
           <Route path="/waiter-dashboard" element={<WaiterDashboard />} />
