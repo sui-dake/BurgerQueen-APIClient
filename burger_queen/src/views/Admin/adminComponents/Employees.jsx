36886be2a629e4347e50cf9527bee7e55cc2d0a3
sui-dake <sui.vf@tuta.io>
2022-06-01 18:24:08 -0500

    erase employee + edit tryout

--- burger_queen/src/views/Admin/adminComponents/Employees.jsx
+++ burger_queen/src/views/Admin/adminComponents/Employees.jsx
@@ -3,10 +3,24 @@
 import { useState, useEffect } from "react";
 import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { db } from "./../../../libs/Firebase-config";
+import Erase from "./Erase";
+import EditEmployees from "./EditEmployees";
 
 export default function Employees() {
   const [employee, setEmployee] = useState([]);
+  const [edit, setEdit] = useState(false);
   const employees = collection(db, "Users");
+
+  const handleEdit = (id) => {
+    if (edit == true) {
+      setEdit(true);
+      console.log(id);
+    } else if (edit == false) {
+      setEdit(true);
+      console.log("id");
+    }
+    //return <div>{edit ? <EditEmployees id={id} /> : null}</div>;
+  };
 
   useEffect(() => {
     onSnapshot(employees, (snapshot) => {
@@ -20,23 +34,70 @@ export default function Employees() {
 
   return (
     <div>
-      <table style={{ border: "none", borderRadius: "20px", padding: "10px" }}>
-        <tr>
-          <th style={{ width: "180px", padding: "10px" }}>Name</th>
-          <th style={{ width: "80px", padding: "10px" }}>Role</th>
-          <th style={{ width: "180px", padding: "10px" }}>E-mail</th>
-        </tr>
-
-        {employee.map((user) => (
+      <div>
+        <table
+          style={{ border: "none", borderRadius: "20px", padding: "10px" }}
+        >
           <tr>
-            <td style={{ width: "180px", padding: "5px 10px" }}>{user.Name}</td>
-            <td style={{ width: "130px", padding: "5px 10px" }}>{user.Role}</td>
-            <td style={{ width: "180px", padding: "5px 10px" }}>
-              {user.Email}
-            </td>
+            <th style={{ width: "180px", padding: "10px" }}>Name</th>
+            <th style={{ width: "80px", padding: "10px" }}>Role</th>
+            <th style={{ width: "180px", padding: "10px" }}>E-mail</th>
           </tr>
-        ))}
-      </table>
+          {employee.map((user) => (
+            <tr>
+              <td
+                style={{
+                  width: "180px",
+                  padding: "5px 10px",
+                  background: "none",
+                  border: "none",
+                  borderBottom: "0px solid black",
+                }}
+                defaultValue={user.Name}
+              >
+                {user.Name}
+              </td>
+              <td
+                style={{
+                  width: "130px",
+                  padding: "5px 10px",
+                  background: "none",
+                  border: "none",
+                  borderBottom: "0px solid black",
+                }}
+                defaultValue={user.Role}
+              >
+                {user.Role}
+              </td>
+              <td
+                style={{
+                  width: "180px",
+                  padding: "5px 10px",
+                  background: "none",
+                  border: "none",
+                  borderBottom: "0px solid black",
+                }}
+                defaultValue={user.Email}
+              >
+                {user.Email}
+              </td>
+              <td style={{ border: "none" }}>
+                <img
+                  id="logo_burger_queen"
+                  type="button"
+                  src="./edit1.png.png"
+                  alt="logo"
+                  onClick={() => handleEdit(user.id)}
+                />
+                {edit ? <EditEmployees id={user.id} /> : null}
+              </td>
+              <td style={{ border: "none" }}>
+                <Erase id={user.id} />
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
